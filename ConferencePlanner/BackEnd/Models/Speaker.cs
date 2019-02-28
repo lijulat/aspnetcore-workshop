b5e545aa53128364214374731ff9632e4706159e
Liju <lijulathief@gmail.com>
2019-02-28 15:11:53 +0100

    Add files via upload

---
+++ ConferencePlanner/BackEnd/Models/Speaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Speaker : ConferenceDTO.Speaker
+    {
+
+    }
+}
